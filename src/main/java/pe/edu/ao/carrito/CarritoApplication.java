5b896a5fdbb177e428af8f84e28fb48eddb660a9
Andres Ocaris <andresocaris@MacBook-Air-de-Andres.local>
2021-09-13 13:40:05 -0500

    agregando seguridad a mi spring boot con token

--- src/main/java/pe/edu/ao/carrito/CarritoApplication.java
+++ src/main/java/pe/edu/ao/carrito/CarritoApplication.java
@@ -2,6 +2,14 @@ package pe.edu.ao.carrito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import pe.edu.ao.carrito.security.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class CarritoApplication {
@@ -10,5 +18,17 @@ public class CarritoApplication {
 		SpringApplication.run(CarritoApplication.class, args);
 		System.out.println("Welcome to carrito");
 	}
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/user").permitAll()
+				.anyRequest().authenticated();
+		}
+	}
 }
